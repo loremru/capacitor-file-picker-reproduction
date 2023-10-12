59a95a82a378475976c9fa9f3ed9f20dc60d34d0
loremru <mr.nonamerz08@yandex.ru>
2023-10-12 15:12:39 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+
+
+<template>
+  <button @click="addVideo">UPLOAD VIDEO</button>
+</template>
+
+<script lang="ts" setup>
+import {FilePicker} from "@capawesome/capacitor-file-picker";
+
+async function addVideo() {
+  try {
+    console.log("SUCCESS: ", (await FilePicker.pickVideos({skipTranscoding: true})).files)
+  } catch (e) {
+    console.log("ERR: ", e)
+  }
+}
+</script>
